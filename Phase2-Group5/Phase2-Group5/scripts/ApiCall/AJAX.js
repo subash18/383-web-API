2283f2e6a85b4eaf7cca280de215ce314dfaac16
cory_clapp <cory.clapp@selu.edu>
2016-02-18 23:22:50 -0600

    added to make the request header compatible with modern jquery requests

--- Phase2-Group5/Phase2-Group5/scripts/ApiCall/AJAX.js
+++ Phase2-Group5/Phase2-Group5/scripts/ApiCall/AJAX.js
@@ -1,11 +1,19 @@
 ﻿var username = $("input#email").val();
 var password = $("input#password").val();
+
+function make_base_auth(user, password) {
+    var tok = username + ":" + password;
+    var hash = btoa(tok);
+    return "Basic " + hash;
+}
 $.ajax({
     type: "GET",
     url: "/api/ApiKey",
     dataTpe: "json",
-    headers: {
-        "Authorization": "Basic " + btoa(username + ":" + password)
+    async: false,
+    data: "{}",
+    beforeSend: function(xhr) {
+        xhr.setRequestHeader("Authorization", make_base_auth(username, password));
     },
     statusCode: {
         403: function() {
