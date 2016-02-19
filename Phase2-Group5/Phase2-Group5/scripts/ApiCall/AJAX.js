3d300e772e1172adfd38743ee62e5a5804c8640a
cory_clapp <cory.clapp@selu.edu>
2016-02-18 23:03:52 -0600

    ajax.js is a ajax call that passes in the email and password to the /api/ApiKey endpoint once its created if you decide to use forms just adjust a little bit

---
+++ Phase2-Group5/Phase2-Group5/scripts/ApiCall/AJAX.js
@@ -0,0 +1,18 @@
+﻿var username = $("input#email").val();
+var password = $("input#password").val();
+$.ajax({
+    type: "GET",
+    url: "/api/ApiKey",
+    dataTpe: "json",
+    headers: {
+        "Authorization": "Basic " + btoa(userame + ":" + password)
+    },
+    statusCode: {
+        403: function() {
+            alert("Incorrect credentials, please try again.");
+        }
+    },
+    success: function(data) {
+        console.log(data);
+    }
+})
