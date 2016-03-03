fa04ce201eb3bd6461cfd578b2bfeaf448394a21
Caleb Carter <caleb.carter@selu.edu>
2016-03-03 00:28:33 -0600

    -- AdminMobileApp (using Xamarin currently will update with the Visual Studio version when its working)
    -- Simple layout of the admin page (its currently set to the main page when the application opens)
    -- No authentication / validation currently (Need to work on getting AuthenticationKey from web API)
    -- Need to work on productions section (currently not in this build - categories / manufacturers fall into this part)

---
+++ AdminMobileApp/AdminMobileApp/AdminMobileApp.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace AdminMobileApp
+{
+	public class App : Application
+	{
+		public App ()
+		{
+			// The root page of your application
+			MainPage = new ContentPage {
+				Content = new StackLayout {
+					Spacing = 20, Padding = 50,
+					VerticalOptions = LayoutOptions.Center,
+					Children = {
+						new Entry { Placeholder = "Username" },
+						new Entry { Placeholder = "Password", IsPassword = true },
+						new Button {
+							Text = "Login",
+							TextColor = Color.White,
+							BackgroundColor = Color.FromHex("77D065")}, 
+						new Label {
+							XAlign = TextAlignment.Center,
+							Text = "Please enter your username and password"
+						}
+					}
+				}
+			};
+					
+		}
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
+
