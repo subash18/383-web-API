46b9f65fc68cff24362c28c22be8fe7b51398e7d
Caleb Carter <caleb.carter@selu.edu>
2016-03-06 17:27:41 -0600

    -- Starting of the application reworked. Made it into a navigation page that sets the LoginPage as the default page.
    -- Now has simple navigation between pages. (Need to check security of certain pages using this method, but it should be okay)
    -- Login button currently set without checking to let the user go to the management page.
    -- Management page needs fleshing out. (Thinking a specific page for users, and a specific page for products)
    -- Logic needs to be worked on and implemented for all pages (Still in the works)

---
+++ AdminMobileApp/AdminMobileApp/LoginPage.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace AdminMobileApp
+{
+	public class LoginPage : ContentPage
+	{
+		public LoginPage ()
+		{
+			var LoginButton = new Button () { Text = "Login", TextColor = Color.White, BackgroundColor = Color.FromHex ("77D065")};
+			LoginButton.Clicked += async (object sender, EventArgs e) => 
+			{
+				await this.Navigation.PushAsync(new ManagementPage());
+			};
+				
+			Content = new StackLayout { 
+				Spacing = 20, Padding = 50,
+				VerticalOptions = LayoutOptions.Center,
+				Children = {
+					new Entry { Placeholder = "Username" },
+					new Entry { Placeholder = "Password", IsPassword = true },
+					LoginButton,
+					//new Button {
+						//Text = "Login",
+						//TextColor = Color.White,
+						//BackgroundColor = Color.FromHex ("77D065"),
+						//Command = new Command(() => NavigationPage.PushAsync(new ManagementPage()))
+	 
+					new Label {
+						XAlign = TextAlignment.Center,
+						Text = "Please enter your username and password"
+					}
+				}
+			};
+		}
+	}}
+
+
---
+++ AdminMobileApp/AdminMobileApp/ManagementPage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace AdminMobileApp
+{
+	public class ManagementPage : ContentPage
+	{
+		public ManagementPage ()
+		{
+			Content = new StackLayout { 
+				Children = {
+					new Label { Text = "This is the management page" }
+				}
+			};
+		}
+	}
+}
+
+
